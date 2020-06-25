002ce24fe02d61c66a934b909f57dd95c27cb17e
Alvin Zablan <azablan.dev@gmail.com>
2020-06-25 11:20:05 -0400

    add layer

--- neural-network/index.js
+++ neural-network/index.js
@@ -1,3 +1,7 @@
+const sigmoid = x => 1 / (1 + Math.E**(-x));
+const getRandom = (min, max) => Math.random() * (max - min) + min;
+const getRandomWeightArray = size => new Array(size).fill().map(() => getRandom(-1, 1));
+
 class Neuron {
   constructor(weights, bias) {
     this.weights = weights;
@@ -10,7 +14,40 @@ class Neuron {
 
     const weightedSum = inputs.reduce((sum, input, idx) => sum + (input * this.weights[idx]), 0);
     return weightedSum + this.bias;
+    return sigmoid(weightedSum + this.bias); // TODO no activation function
   }
 }
 
+class NeuralLayer {
+  constructor(numInputs, numNeurons) {
+    this.neurons = new Array(numNeurons).fill().map(
+      () => new Neuron(getRandomWeightArray(numInputs), 0)
+    );
+  }
 
+  forward(inputs) {
+    this.outputs = this.batchActivate(inputs);
+  }
+
+  batchActivate(batchInputs) {
+    return batchInputs.map(input => this.activate(input));
+  }
+
+  activate(inputs) {
+    return this.neurons.map(neuron => neuron.activate(inputs));
+  }
+}
+
+const input = [
+  [1, 2, 3, 2.5],
+  [2.0, 5.0, -1.0, 2.0],
+  [-1.5, 2.7, 3.3, -0.8],
+];
+
+h1 = new NeuralLayer(4, 5);
+h2 = new NeuralLayer(5, 5);
+
+h1.forward(input);
+console.log(h1.outputs);
+h2.forward(h1.outputs);
+console.log(h2.outputs);
